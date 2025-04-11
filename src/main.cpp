07129b45d16061354fc2c1e99bf635baafbe7e8b
Felo Mousa <felogmousa@gmail.com>
2025-04-11 15:59:46 -0700

    finance api endpoint +  json

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <ArduinoJson.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
 #define OLED_RESET   -1
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const char* ssid     = "";
 const char* password = "";
@@ -25,38 +21,45 @@ void connectWiFi() {
   Serial.println("\nwifi connected!");
 }
 
+void getData() {
+  if (WiFi.status() != WL_CONNECTED)
+    return;
+    
+  HTTPClient http;
+  http.begin(endpoint);
+  int code = http.GET();
+  if (code == HTTP_CODE_OK) {
+    String payload = http.getString();
+    StaticJsonDocument<256> doc;
+    if (!deserializeJson(doc, payload)) {
+      float price = doc["current_price"];
+      float change = doc["daily_change"];
+      char line1[16], line2[16];
+      snprintf(line1, sizeof(line1), "$%.2f", price);
+      
+      if (change >= 0) // if price change >0, show with "+", otherwise use "-"
+        snprintf(line2, sizeof(line2), "+$%.2f", change);
+      else
+        snprintf(line2, sizeof(line2), "-$%.2f", -change);
+        
+      Serial.println(line1);
+      Serial.println(line2);
+    } else {
+      Serial.println("json parse error");
+    }
+  } else {
+    Serial.print("http error: ");
+    Serial.println(http.errorToString(code));
+  }
+  http.end();
+}
+
 void setup() {
   Serial.begin(115200);
   connectWiFi();
 }
 
-// void setup() {
-//   Serial.begin(115200);
-//   Wire.begin();
-//   display.begin(SSD1306_SWITCHCAPVCC);
-
-//   display.clearDisplay();
-//   display.setTextSize(4);
-//   display.setTextColor(SSD1306_WHITE);
-
-//   display.setCursor(0, 2); // vertically centered
-
-//   display.display();
-//   delay(1000);
-
-//   Serial.println("setup donzo");
-// }
-
 void loop() {
-  // display.setTextColor(SSD1306_INVERSE);
-
-  // display.clearDisplay();
-  // display.setCursor(0, 2); // vertically centered
-
-  // display.print(counter);
-  // display.display();
-  // //Serial.println(counter);
-  // delay(100);
-  // counter++;
-  // if (counter > 100) counter = 1;
+  getData();
+  delay(5000);
 }
