e909149080e5872759da796ea7ae661c9f13eb00
Felo Mousa <felogmousa@gmail.com>
2025-04-11 15:53:11 -0700

    added wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,62 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <ArduinoJson.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
-#define RESET_PIN    -1 // not in use
+#define OLED_RESET   -1
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, RESET_PIN);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-int counter = 1;
+const char* ssid     = "";
+const char* password = "";
+const char* endpoint = "https://api.felomousa.com/finance";
+
+void connectWiFi() {
+  Serial.print("connecting to wifi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nwifi connected!");
+}
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
-  display.begin(SSD1306_SWITCHCAPVCC);
-  display.clearDisplay();
-
-  delay(1000);
-  Serial.println("setup donzo");
+  connectWiFi();
 }
 
-void loop() {
-  display.clearDisplay();
-  display.setCursor(0, 2); // vertically centered
+// void setup() {
+//   Serial.begin(115200);
+//   Wire.begin();
+//   display.begin(SSD1306_SWITCHCAPVCC);
 
-  display.print(counter);
-  display.display();
-  //Serial.println(counter);
-  counter++;
-  if (counter > 100) counter = 1;
-  delay(100);
+//   display.clearDisplay();
+//   display.setTextSize(4);
+//   display.setTextColor(SSD1306_WHITE);
+
+//   display.setCursor(0, 2); // vertically centered
+
+//   display.display();
+//   delay(1000);
+
+//   Serial.println("setup donzo");
+// }
+
+void loop() {
+  // display.setTextColor(SSD1306_INVERSE);
+
+  // display.clearDisplay();
+  // display.setCursor(0, 2); // vertically centered
+
+  // display.print(counter);
+  // display.display();
+  // //Serial.println(counter);
+  // delay(100);
+  // counter++;
+  // if (counter > 100) counter = 1;
 }
