fa5cf1b9def180bca031cdb72974f0bd2835e6bc
Felo Mousa <felogmousa@gmail.com>
2025-04-11 14:54:46 -0700

    basic screen write + setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define RESET_PIN    -1 // not in use
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, RESET_PIN);
+
+int counter = 1;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Wire.begin();
+  display.begin(SSD1306_SWITCHCAPVCC);
+
+  display.clearDisplay();
+  display.setTextSize(4);
+  display.setTextColor(SSD1306_WHITE);
+
+  display.setCursor(0, 2); // vertically centered
+
+  display.display();
+  delay(1000);
+
+  Serial.println("setup donzo");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  display.setTextColor(SSD1306_INVERSE);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  display.clearDisplay();
+  display.setCursor(0, 2); // vertically centered
+
+  display.print(counter);
+  display.display();
+  //Serial.println(counter);
+  delay(100);
+  counter++;
+  if (counter > 100) counter = 1;
 }
